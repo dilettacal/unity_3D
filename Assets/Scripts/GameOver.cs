67d623fed05f06719afa0fe453ee649a677da1f1
Di Cal <diletta.cal@gmail.com>
2018-07-16 23:01:00 +0200

    New Scene
    Added new scene with cubes and ball

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+    private string SCENE_NAME = "DiScene";
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < -5.0f) //diamo tempo al giocatore di capire che sta cadendo
+        {
+            Application.LoadLevel(SCENE_NAME);
+        }
+	}
+}
