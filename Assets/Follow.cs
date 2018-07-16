de316134c3d1061d94ec67273d917c56d06a46c2
Di Cal <diletta.cal@gmail.com>
2018-07-16 18:24:32 +0200

    Player ball and obstacle1
    Added Player as ball

---
+++ Assets/Follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public GameObject target; //Target of the camera follow
+    private Vector3 distance; //between camera and object
+
+	// Use this for initialization
+	void Start () {
+        distance = transform.position - target.transform.position; //Difference camera and target
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.position = target.transform.position + distance;
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //bewegung vom Benutzer ueber x und y Achse
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 input = new Vector3(h, 0.0f, v);
+        //Modify speed and direction on the inputs
+        rigidbody.AddForce(speed * input);
+	}
+}
