70164ed6b8c8cb0dc1c86d33dd1180410044ecbc
Di Cal <diletta.cal@gmail.com>
2018-07-18 16:37:23 +0200

    Basic movement for ghost
    Gegner bewegt sich rechts-links, up and down

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float distance = 2f; //Beweungsdistanz
+    public bool moveUp = false;
+    public bool moveSide = true; //rechts und links moeglich
+
+    Vector3 startingPos;
+
+    Transform trans; //Speichert die Position von Feind
+
+
+	// Use this for initialization
+	void Start () {
+        trans = GetComponent<Transform>(); //initialisiert die Position
+        startingPos = trans.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (moveUp)
+        {
+            //ping pong Funktion bewegt nach oben und nach unten
+            trans.position = new Vector3(startingPos.x, startingPos.y + Mathf.PingPong(Time.time, distance), startingPos.z);
+        }
+        if (moveSide)
+        {
+            //ping pong Funktion bewegt hin und her
+            trans.position = new Vector3(startingPos.x + Mathf.PingPong(Time.time, distance), startingPos.y , startingPos.z);
+        }
+		
+	}
+}
