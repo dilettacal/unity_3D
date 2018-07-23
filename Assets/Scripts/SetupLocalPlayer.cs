38c3c631eb32a1eb5bec111479f69b250fff0d46
Di Cal <diletta.cal@gmail.com>
2018-07-23 12:51:57 +0200

    Added spawn point for player

---
+++ Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+//Steuert den Lokalen Player (also im lokalen Fenster)
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        if (isLocalPlayer)
+        {
+            //this Goblin wird vom aktuellen Benutzer verwendet
+            GetComponent<GoblinMovement>().enabled = true;
+        }
+        else
+        {
+            //this Goblin wird von einem anderen Benutzer verwendet
+            GetComponent<GoblinMovement>().enabled = false;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
