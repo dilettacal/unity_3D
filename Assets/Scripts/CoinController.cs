4a25b62f8d01bd4fd5232769c207a9e9db3f18a6
rockie-xu <klenovyj7list@gmail.com>
2018-07-24 19:32:28 +0200

    little commit 02

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -15,6 +15,31 @@ public class CoinController : MonoBehaviour {
 	void Update () {
         float angle = rotationSpeed * Time.deltaTime; //time from last call of this function
         transform.Rotate(Vector3.up * angle, Space.World); //Coin rotates direction up with angle angle in the actual world
-		
+        //coinsGenerate(6);
 	}
+
+    void coinsGenerate(int anzahl)
+    {
+        //MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+        //Resolution terrainSize = gameObject.get
+
+        /*        float minX = renderer.bounds.min.x;
+                float maxX = renderer.bounds.max.x;
+                float minZ = renderer.bounds.min.z;
+                float maxZ = renderer.bounds.max.x;
+                
+                float newX = Random.Range(50, 750);
+                float newZ = Random.Range(50, 750);
+                float newY = gameObject.transform.position.y+7;
+
+                int i = 0;
+                while(i < anzahl)
+                {
+                    GameObject coin = GameObject.FindGameObjectWithTag("Coin");
+                    coin.transform.position = new Vector3(newX, newY, newZ);
+                }
+                
+        Debug.Log("Anzahl ist: "+anzahl); */
+        
+    }
 }
